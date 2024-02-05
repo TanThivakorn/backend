3df970289560c6a69e6e0fcfbe17f61b13a61d10
Thivakorn82 <thivakorn.n@ku.th>
2024-02-05 21:26:25 +0700

    environment

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const express = require("express");
 
 const cors = require("cors");
 const app = express();
-const port = 8080;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient } = require("mongodb");
 
+const port = process.env.PORT || 8080;
+const databaseUrl = process.env.MONGO_DB;
+
 app.post("/test/timestamp", async (req, res) => {
   const thailandTimeZone = "Asia/Bangkok";
   const options = { timeZone: thailandTimeZone };
 
   const currentDateTime = new Date().toLocaleString("en-US", options);
-  const client = new MongoClient(
-    "mongodb+srv://thivakornng:cQb60qeAB2m5HrU4@foryouonly.4etocee.mongodb.net/"
-  );
+  const client = new MongoClient(databaseUrl);
   await client.connect();
   await client.db("foryouonly").collection("timestamp").insertOne({
     time_stamp: currentDateTime,
