cd98fa563e4dbbdc6020310c2aac56a7a19ae86c
Thivakorn82 <thivakorn.n@ku.th>
2024-02-05 21:11:28 +0700

    store time zone to db

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+const express = require("express");
 
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3000
+const cors = require("cors");
+const app = express();
+const port = 8080;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const { MongoClient } = require("mongodb");
+
+app.post("/test/timestamp", async (req, res) => {
+  const thailandTimeZone = "Asia/Bangkok";
+  const options = { timeZone: thailandTimeZone };
+
+  const currentDateTime = new Date().toLocaleString("en-US", options);
+  const client = new MongoClient(
+    "mongodb+srv://thivakornng:cQb60qeAB2m5HrU4@foryouonly.4etocee.mongodb.net/"
+  );
+  await client.connect();
+  await client.db("foryouonly").collection("timestamp").insertOne({
+    time_stamp: currentDateTime,
+  });
+  await client.close();
+  res.status(200).send({
+    status: "ok",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
