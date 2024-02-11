a5660014582e833f79512d932519eb3da1b4f05f
Thivakorn82 <thivakorn.n@ku.th>
2024-02-11 18:38:00 +0700

    remove console log

--- index.js
+++ index.js
@@ -15,23 +15,17 @@ const databaseUrl = process.env.MONGO_DB;
 app.post("/test/timestamp", async (req, res) => {
   const thailandTimeZone = "Asia/Bangkok";
   const options = { timeZone: thailandTimeZone };
-  console.log("11111");
   const currentDateTime = new Date().toLocaleString("en-US", options);
-  console.log("22222");
 
   const client = await MongoClient.connect(databaseUrl);
-  console.log("database url =>" + databaseUrl);
 
   // await client.connect();
-  console.log("clinet =>" + client);
 
   await client.db("foryouonly").collection("timestamp").insertOne({
     time_stamp: currentDateTime,
   });
-  console.log("33333");
 
   await client.close();
-  console.log("44444");
   return res.status(200).send({
     status: "ok",
   });
